4584254ba2c2e6f8b8d00639325ba34aef35fb49
Aléctor Alexander <alector.alexandre@gmail.com>
2021-12-02 19:37:18 -0300

    Desafios 1, 2 e 3 done

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(sim, ounao) {
+  if(sim == true && ounao == true){
+    return true
+  }else{
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+ var returno = base * height / 2;
+ return returno;
 }
 
 // Desafio 3
