83b3f49c56277347e6b5d3928862ee7a36587149
Aléctor Alexander <alector.alexandre@gmail.com>
2021-12-03 20:38:04 -0300

    challenge and challenge2  already DONE. WE ARE CHAMPIONS

--- src/challenges2.js
+++ src/challenges2.js
@@ -105,9 +105,30 @@ function triangleCheck(lineA,lineB,lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+//Referência: https://www.youtube.com/watch?v=pfkkdzeyx6U
+function hydrate(cachaca) {
+ 
+
+  let bebida = cachaca;
+  let reg = /\d+/g;
+  let qtdAlcool = cachaca.match(reg);
+  var numeros = 0;
+  for (let index = 0; index < qtdAlcool.length; index++) {
+    for (let i = 1; i <= 9; i++) {
+      if (i == qtdAlcool[index]){
+        numeros += i;
+
+      }
+      
+    }
+    
+  }
+  if (numeros == 1) {
+    resultado = numeros + " copo de água"
+  } else {
+  resultado = numeros + " copos de água";}
+ return  resultado;
+} 
 
 module.exports = {
   generatePhoneNumber,
